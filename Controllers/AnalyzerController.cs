18e2d62ca68df1c6a2c548d5fdd72afa6070fd42
John Rich Nicolas <johnrichnicolas@gmail.com>
2025-12-08 00:29:18 +0800

    created a controller to gather the input of the user

---
+++ Controllers/AnalyzerController.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using TM_MULTIHEAD_PHISHING_DETECTOR.Models;
+
+namespace TM_MULTIHEAD_PHISHING_DETECTOR.Controllers
+
+{
+    public class AnalyzerController : Controller
+    {
+        [HttpPost]
+        public IActionResult Analyze(string postText)
+        {
+            if (string.IsNullOrWhiteSpace(postText))
+            {
+                return RedirectToAction("Index");
+            }
+                
+            var engine = new MHTMEngine();
+            //var result = engine.Process(postText);
+
+            //return View("Result", result);
+        }
+    }
+}

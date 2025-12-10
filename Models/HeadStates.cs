8aa007f699d0bc0bc3bd94ab710d990b1cc0aa94
John Rich Nicolas <johnrichnicolas@gmail.com>
2025-12-11 00:24:43 +0800

    added the head states

---
+++ Models/HeadStates.cs
@@ -0,0 +1,13 @@
+﻿namespace TM_MULTIHEAD_PHISHING_DETECTOR.Models
+{
+    public class HeadStates
+    {
+        public enum HeadState
+        {
+            q0,          // Start state
+            q1,          // One suspicious indicator found
+            q2_accept,   // Suspicious
+            q_reject     // Credible
+        }
+    }
+}
